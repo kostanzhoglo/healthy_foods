247433c0f001cee15497ed340c91a0282ab0b1f0
kostanzhoglo <jeffreywithers@gmail.com>
2018-01-31 14:07:57 -0500

    Add case statement, while loop and exit clause to #menu in cli.rb

--- lib/healthy_foods/cli.rb
+++ lib/healthy_foods/cli.rb
@@ -16,8 +16,21 @@ class HealthyFoods::CLI
   end
 
   def menu
-    puts "Please enter the number of the food you'd like to learn more about."
-    user_input = gets.strip
+    user_input = nil
+    while user_input != "exit"
+      puts "Please enter the number of the food you'd like to learn more about or press exit to end program."
+      user_input = gets.strip
+      case user_input
+        when "1"
+          puts "Bananas are yellow"
+        when "2"
+          puts "Carrots are orange"
+        when "3"
+          puts "Beans are a variety of colors"
+        when "4"
+          puts "Chicken is a meat"
+      end
+    end
   end
 
 end
