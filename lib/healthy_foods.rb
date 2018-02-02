af929728e366d79434a717ece334d9c4e998b8eb
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-02 09:40:09 -0500

    Reaarange require_relatives in healthy_foods.rb (mistake, unnecessary), and Add first scrape iterator to #scrape_index_page

--- lib/healthy_foods.rb
+++ lib/healthy_foods.rb
@@ -3,9 +3,10 @@ require "nokogiri"
 require "pry"
 
 require_relative "./healthy_foods/version"
-require_relative "./healthy_foods/food"
 require_relative "./healthy_foods/scraper"
+require_relative "./healthy_foods/food"
 require_relative "./healthy_foods/cli"
+
 
 module HealthyFoods
   # Your code goes here...
--- lib/healthy_foods/scraper.rb
+++ lib/healthy_foods/scraper.rb
@@ -1,9 +1,16 @@
 
 class HealthyFoods::Scraper
 
-  def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
+  def self.scrape_index_page
+    index_page = Nokogiri::HTML(open("http://www.whfoods.com/foodstoc.php"))
+    # binding.pry
     food_list = []
+    index_page.css(".blist").each do |line|
+      line.css("li").each do |food|
+        food_name = food.css("a").children.text
+        binding.pry
+      end
+    end
   end
 
   def self.scrape_food_page(food_url)
@@ -11,3 +18,8 @@ class HealthyFoods::Scraper
   end
 
 end
+
+
+
+#  LINE 9 once upon a time
+# food_name = food.css("li").children.text.split(", ")
