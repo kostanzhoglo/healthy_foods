1a8be7a2f5bcd167680a08d47a0d995676f61435
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-05 11:11:47 -0500

    Rearrange a couple lines in scraper.rb, to show example of correct binding.pry placement in blog post.  Put everything back as before. Project finished.  Refactoring coming up

--- lib/healthy_foods/scraper.rb
+++ lib/healthy_foods/scraper.rb
@@ -18,11 +18,11 @@ class HealthyFoods::Scraper
     food_page = Nokogiri::HTML(open(food_url))
     food = {}
     food[:general_info] = food_page.css("p")[3, 2].text
-          # food_page.css("div.slot-6-7-8 div div div").each do |div|
-            # food[:serving_size] = div.css("div").text.gsub("1.00 cup(", " ").gsub(")", "")
-            # food[:calories] = div.css("div").text.gsub(/.+[)]/, "").gsub(/[GI]\S+\s+\S+\s\S+/, "")
-            # binding.pry
-          # end
+    # food_page.css("div.slot-6-7-8 div div div").each do |div|
+    #   food[:serving_size] = div.css("div").text.gsub("1.00 cup(", " ").gsub(")", "")
+    #   food[:calories] = div.css("div").text.gsub(/.+[)]/, "").gsub(/[GI]\S+\s+\S+\s\S+/, "")
+    #   binding.pry
+    # end
     food[:serving_size] = food_page.css("div.slot-6-7-8 div div div div")[0].text.gsub(/.+[(]|[)]/, "")
     food[:calories] = food_page.css("div.slot-6-7-8 div div div div")[1].text.gsub(/[GI]\S+\s+\S+\s\S+/, "")
       # ATTEMPT 2    (/.+[)]|[GI]\S+\s+\S+\s\S+/, "")
