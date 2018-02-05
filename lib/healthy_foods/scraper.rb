f736e1c7b7933740dfc733562d9e6b036901a9f0
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-05 09:28:39 -0500

    Cleaned up scraper.rb. Deleted some old comments, and re-ordered a few old commented out lines, to remember what I did at first

--- lib/healthy_foods/scraper.rb
+++ lib/healthy_foods/scraper.rb
@@ -22,22 +22,12 @@ class HealthyFoods::Scraper
             # food[:serving_size] = div.css("div").text.gsub("1.00 cup(", " ").gsub(")", "")
             # food[:calories] = div.css("div").text.gsub(/.+[)]/, "").gsub(/[GI]\S+\s+\S+\s\S+/, "")
             # binding.pry
-      # TESTING THIS CODE.
+          # end
     food[:serving_size] = food_page.css("div.slot-6-7-8 div div div div")[0].text.gsub(/.+[(]|[)]/, "")
     food[:calories] = food_page.css("div.slot-6-7-8 div div div div")[1].text.gsub(/[GI]\S+\s+\S+\s\S+/, "")
-    # ATTEMPT 2    (/.+[)]|[GI]\S+\s+\S+\s\S+/, "")
-    # ATTEMPT 1  .gsub(/[GI]\S+\s+\S+\s\S+/, "")
-          # end
-    # binding.pry
+      # ATTEMPT 2    (/.+[)]|[GI]\S+\s+\S+\s\S+/, "")
+      # ATTEMPT 1  .gsub(/[GI]\S+\s+\S+\s\S+/, "")
     food
   end
 
 end
-
-
-
-#  LINE 9 once upon a time. Didn't return what I needed but was close.
-# food_name = food.css("li").children.text.split(", ")
-
-# Fill in for Food_url when doing tests in # scrape_food_page
-# "http://www.whfoods.com/genpage.php?tname=foodspice&dbid=84"
