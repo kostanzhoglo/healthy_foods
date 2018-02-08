57cc76829b5eed4b4a4c0eee65f0e085a16bc999
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-08 06:13:47 -0500

    Put index_page and food_page back to local variables in scraper.rb

--- lib/healthy_foods/scraper.rb
+++ lib/healthy_foods/scraper.rb
@@ -2,9 +2,9 @@
 class HealthyFoods::Scraper
 
   def self.scrape_index_page
-    @index_page = Nokogiri::HTML(open("http://www.whfoods.com/foodstoc.php"))
+    index_page = Nokogiri::HTML(open("http://www.whfoods.com/foodstoc.php"))
     food_list = []
-    @index_page.css(".blist").each do |line|
+    index_page.css(".blist").collect do |line|
       line.css("li").each do |food|
         food_name = food.css("a").children.text
         food_url = food.css("a").attribute("href").value
@@ -15,16 +15,16 @@ class HealthyFoods::Scraper
   end
 
   def self.scrape_food_page(food_url)
-    @food_page = Nokogiri::HTML(open(food_url))
+    food_page = Nokogiri::HTML(open(food_url))
     food = {}
-    food[:general_info] = @food_page.css("p")[3, 2].text
+    food[:general_info] = food_page.css("p")[3, 2].text
     # food_page.css("div.slot-6-7-8 div div div").each do |div|
     #   food[:serving_size] = div.css("div").text.gsub("1.00 cup(", " ").gsub(")", "")
     #   food[:calories] = div.css("div").text.gsub(/.+[)]/, "").gsub(/[GI]\S+\s+\S+\s\S+/, "")
     #   binding.pry
     # end
-    food[:serving_size] = @food_page.css("div.slot-6-7-8 div div div div")[0].text.gsub(/.+[(]|[)]/, "")
-    food[:calories] = @food_page.css("div.slot-6-7-8 div div div div")[1].text.gsub(/[Calories: ]|[GI].+/, "") 
+    food[:serving_size] = food_page.css("div.slot-6-7-8 div div div div")[0].text.gsub(/.+[(]|[)]/, "")
+    food[:calories] = food_page.css("div.slot-6-7-8 div div div div")[1].text.gsub(/[Calories: ]|[GI].+/, "")
       # ATTEMPT 2    (/.+[)]|[GI]\S+\s+\S+\s\S+/, "")
       # ATTEMPT 1  .gsub(/[GI]\S+\s+\S+\s\S+/, "")
     food
